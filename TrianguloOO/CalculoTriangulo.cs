c30efdad3de7ac9fbdd5af8e79b70f4bb31315dc
Arthur Leonardo Filipus <ArthurFilipus@hotmail.com>
2023-04-06 14:36:03 -0300

    Add project files.

---
+++ TrianguloOO/CalculoTriangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOO
+{
+    internal class CalculoTriangulo
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public string triangulo { get; set; }
+        public string CalcularTriangulos()
+        {
+            if (x == y && y == z && x == z)
+            {
+                return triangulo = "Triangulo Equilatero";
+            }
+            if (x != y && y != z && x != z)
+            {
+                return triangulo = "Triangulo Escaleno";
+            }
+            else
+            {
+                return triangulo = "Triangulo Isoceles";
+            }
+        }
+    }
+}
---
+++ TrianguloOO/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TrianguloOO
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculoTriangulo teste = new CalculoTriangulo();
+
+
+
+
+
+            Console.Write("Digite o valor de X: ");
+
+            teste.x = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Digite o valor de Y: ");
+
+            teste.y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor de Z: ");
+
+            teste.z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(teste.CalcularTriangulos());
+
+            Console.ReadLine();
+        }
+    }
+}
